b46d1cfb4c6929e054074816ae297310c0087607
edneyRoldao <edneyroldao@gmail.com>
2017-08-08 17:13:30 -0300

    test

--- toastr-alert-test/public/js/controllers/controller.js
+++ toastr-alert-test/public/js/controllers/controller.js
@@ -1,8 +1,31 @@
 angular.module('app').controller('Controller', function($scope, toastr) {
 	$scope.welcome = "test toastr alert";
 
-	$scope.test = function() {
+	$scope.example1 = function() {
 		toastr.success("Hello", "Guy !!!!");
 	};
+
+	$scope.example2 = function() {
+		toastr.info("Hello", "Guy !!!!");
+	};
+
+	$scope.example3 = function() {
+		toastr.error("Hello", "Guy !!!!");
+	};
+
+	$scope.example4 = function() {
+		toastr.warning("Hello", "Guy !!!!");
+	};
+
+	$scope.closeAlert = function() {
+		toastr.clear();
+	};
+
+	$scope.storeToast = function() {
+		var toast = toastr.error("you are not allowed to do this!");
+		// do something...
+		toastr.refreshTimer(toast);
+	};
+
 
 });
