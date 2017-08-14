a32731c408e3e2ec98f3f48b9124fdb08160515f
edneyRoldao <edneyroldao@gmail.com>
2017-08-13 21:32:02 -0300

    test

--- momentjs-tests/public/js/controllers/controller.js
+++ momentjs-tests/public/js/controllers/controller.js
@@ -2,12 +2,18 @@ angular.module('app').controller('Controller', function($scope) {
 	$scope.test = [];
 	$scope.welcome = "Hello Moment JS !";
 
-	var maxYear = moment().years() - 90; 
-	var minYear = moment().subtract(16, "years").years();
+	var maxYear = moment().year() - 90; 
+	var minYear = moment().subtract(16, "years").year();
 
 
 	for(var i = minYear; i > maxYear; i--) {
 		$scope.test.push(i);
 	}
+
+	var data = moment([1982, 0, 25]);
+
+	console.log( data.date() );
+	console.log( data.month() );
+	console.log( data.year() );
 
 });
